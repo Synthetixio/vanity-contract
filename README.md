55fe0fac05a3bc41dd99e164ea4a81b976d19529
Jackson C <jackson@synthetix.io>
2019-09-23 15:06:11 +1000

    Add vanity-contract and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # vanity-contract
 Tool to mine vanity ETH contract address
+
+Uses ethereumjs-wallet to randomly generate wallet and checks first contract address created at nonce 0 would match pattern.
+
+Prints out wallet address + privateKey.
+
+# Edit address pattern to find
+
+Address pattern to find can be changed in index.js, ie 0x5e74.....
+
+```
+const pattern = new RegExp('^5e74'); // replace pattern to find
+```
+
+# Run in PM2 cluster mode
+
+Run in PM2 cluster mode to run on multi-CPU for faster chance to mine address
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const rlp = require('rlp');
+const keccak = require('keccak');
+const wallet = require('ethereumjs-wallet');
+
+const pattern = new RegExp('^5e74'); // replace pattern to find
+let found = false;
+let runs = 0;
+
+while (!found) {
+  if (runs % 1e5 == 0) {
+    console.log(`runs: ${runs}`);
+  }
+  let nonce = 0x00; //The nonce must be a hex literal!
+  let sender = wallet.generate(); // Generate wallet
+  let address = sender.getAddressString(); //Requires a hex string as input!
+
+  let input_arr = [address, nonce];
+  let rlp_encoded = rlp.encode(input_arr);
+
+  let contract_address_long = keccak('keccak256')
+    .update(rlp_encoded)
+    .digest('hex');
+
+  let contract_address = contract_address_long.substring(24); //Trim the first 24 characters.
+
+  if (pattern.test(contract_address)) {
+    const privateKey = sender.getPrivateKeyString();
+    console.log(`contract_address: ${contract_address}`);
+    console.log(`sender address: ${address}`);
+    console.log(`privateKey: ${privateKey}`);
+    found = true;
+  }
+  runs++;
+}
